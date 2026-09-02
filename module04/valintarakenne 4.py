fb7c74f497f7cac36cb48f1775262d3bf2cc4c4d
amitar396 <amit.aryal@metropolia.fi>
2026-09-02 22:22:55 +0300

    Delete module04 directory

--- module04/valintarakenne 4.py
+++
@@ -1,6 +0,0 @@
-import math
-vuosi = int(input("kerro vuosiluku: "))
-if vuosi % 4 == 0 and vuosi % 100 != 0 or vuosi % 400 == 0:
-    print("Tama on karkausvuosi.")
-else:
-    print("Annettu vuosi ei ole karkausvuosi.")
